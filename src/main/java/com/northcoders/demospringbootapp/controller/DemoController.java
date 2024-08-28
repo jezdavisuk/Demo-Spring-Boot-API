b0fe63d3d29e950d1b8d53cb520a5f706272d4f9
Alan Davison <contact@decidedlyhuman.co.uk>
2024-08-28 12:08:59 +0100

    Implement external API search.

--- src/main/java/com/northcoders/demospringbootapp/controller/DemoController.java
+++ src/main/java/com/northcoders/demospringbootapp/controller/DemoController.java
@@ -1,8 +1,15 @@
 package com.northcoders.demospringbootapp.controller;
 
+import com.northcoders.demospringbootapp.api_helpers.GenericDAO;
+import com.northcoders.demospringbootapp.model.Location;
+import com.northcoders.demospringbootapp.model.LocationResponse;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Optional;
 
 @RestController()
 @RequestMapping("api/v1")
@@ -16,5 +23,17 @@ public class DemoController {
     @GetMapping("/hello")
     private String getHello() {
         return "<html><img src=\"https://media.tenor.com/WuOwfnsLcfYAAAAC/star-wars-obi-wan-kenobi.gif\" /></html>";
+    }
+
+    @GetMapping("/search/location/{searchCity}")
+    private ArrayList<Location> getLocationInfo(@PathVariable String searchCity) {
+        String jsonResponse = GenericDAO.getJsonResponse(
+                "https://geocoding-api.open-meteo.com/v1/search",
+                new String[] {"name=" + searchCity, "count=2", "language=en", "format=json"},
+                new String[] {});
+
+        Optional<LocationResponse> response = GenericDAO.mapJsonToPojo(jsonResponse, LocationResponse.class);
+
+        return response.get().results();
     }
 }
--- src/main/java/com/northcoders/demospringbootapp/model/LocationResponse.java
+++ src/main/java/com/northcoders/demospringbootapp/model/LocationResponse.java
@@ -2,5 +2,5 @@ package com.northcoders.demospringbootapp.model;
 
 import java.util.ArrayList;
 
-public record LocationResponse(ArrayList<Location> results) {
+public record LocationResponse(ArrayList<Location> results, String generationtime_ms) {
 }
